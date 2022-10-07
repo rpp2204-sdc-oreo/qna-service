04e3d534c87aabe500bef138be5d3737f21dac33
William Kent <kentwl1876@gmail.com>
2022-10-06 20:23:39 -0800

    Set up a more proper connection to the database and fully set up the schema for the tables. Have yet to figure out a way to run the .sql file, but I have created the tables by copy/pasting.

--- pgSchema.sql
+++ pgSchema.sql
@@ -1,38 +1,42 @@
 /*Basic schema for the qna service. You should be able to run this in the terminal to
   automatically setup the tables in the database.*/
 
+/*NOTE: Currently can't find a method to run this file and need to look into it more.*/
+
 /*Select Database*/
 \c qna;
 
-/*Create the tables in the database*/
+/*Create the tables in the database. May need to copy/paste these individually into psql
+  in order to create the tables*/
 CREATE TABLE IF NOT EXISTS products (
-  product_id SERIAL UNIQUE,
-  PRIMARY KEY (product_id)
+  id SERIAL PRIMARY KEY,
+  product_id TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS questions (
-  product_id INTEGER REFERENCES products (product_id),
-  question_id SERIAL UNIQUE,
+  id SERIAL PRIMARY KEY,
+  product_id INTEGER REFERENCES products (id),
+  question_id TEXT NOT NULL,
   asker_name VARCHAR(50) NOT NULL,
   question_body TEXT NOT NULL,
   question_date TIMESTAMP NOT NULL,
   helpfulness INTEGER NOT NULL,
-  reported BOOLEAN NOT NULL,
-  PRIMARY KEY (question_id)
+  reported BOOLEAN NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS answers (
-  question_id INTEGER REFERENCES questions (question_id),
-  answer_id SERIAL UNIQUE,
+  id SERIAL PRIMARY KEY,
+  question_id INTEGER REFERENCES questions (id),
+  answer_id TEXT NOT NULL,
   answerer_name VARCHAR(50) NOT NULL,
   answer_body TEXT NOT NULL,
   answer_date TIMESTAMP NOT NULL,
-  reported BOOLEAN NOT NULL,
-  PRIMARY KEY (answer_id)
+  reported BOOLEAN NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS photos (
-  photo_id SERIAL UNIQUE,
-  answer_id INTEGER REFERENCES answers (answer_id),
+  id SERIAL UNIQUE,
+  photo_id TEXT NOT NULL,
+  answer_id INTEGER REFERENCES answers (id),
   link TEXT NOT NULL
 );
--- server/db.js
+++ server/db.js
@@ -1,11 +1,26 @@
 require('dotenv').config();
-const {Client} = require('pg');
+const {Pool, Client} = require('pg');
 
 const USER = process.env.USERNAME;
 const PASS = process.env.PASS;
 
 
-const client = new Client({
+// const client = new Client({
+//   user: USER,
+//   host: 'localhost',
+//   database: 'qna',
+//   password: PASS,
+//   port: 5432
+// });
+
+// client.connect();
+
+// client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+//   console.log(err ? err.stack : res.rows[0].message);
+//   client.end();
+// });
+
+const Pool = new Pool({
   user: USER,
   host: 'localhost',
   database: 'qna',
@@ -13,9 +28,18 @@ const client = new Client({
   port: 5432
 });
 
-client.connect();
-
-client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
-  console.log(err ? err.stack : res.rows[0].message);
-  client.end();
+pool.on('error', (err, client) => {
+  console.error('ERROR:', err);
 });
+
+module.exports = {
+  query: (query, params, cb) => {
+    // NOTE: For running a single query
+    return pool.query(text, params, callback);
+  },
+  getClient: (callback) => {
+    //NOTE: For running multiple queries from a single client
+    pool.connect((err, client, release) => {
+      callback(err, client, release);
+    });
+  };
